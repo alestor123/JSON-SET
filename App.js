a85e6380a216910722e9895460d1e9d93c177b46
alestor123 <alestoraldous@gmail.com>
2021-03-24 12:38:12 +0530

    added main code

--- App.js
+++ App.js
@@ -1,4 +1,8 @@
 var {readFileSync,writeFileSync} = require('fs')
-module.exports = (path,data) => {
-
+module.exports = (path,main) => {
+if(!(path&&main)) throw Error('Please enter the path and data')
+var data = JSON.parse(readFileSync(path).toString()),
+spl = main.split((/[=]/));
+data[spl[0]] = spl[1]
+writeFileSync('./lol.json',JSON.stringify(data))
 }
